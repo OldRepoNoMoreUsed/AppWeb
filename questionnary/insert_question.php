3438ec0f805f20b92de3f3d452cbae72f6b5c318
Nicolas Sommer <Nicolas Sommer>
2016-05-06 08:50:34 +0200

    Ajout graphique

--- questionnary/insert_question.php
+++ questionnary/insert_question.php
@@ -31,73 +31,154 @@ if(!isset($_SESSION['user'])){
         <title>Nouvelle question</title>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     </head>
-    <style type="text/css">
-    form
-    {
-        text-align:center;
-    }
+    <style>
+        *{
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+        html{
+            height: 100%;
+        }
+        body{
+            position: relative;
+            min-height: 100%;
+            margin: 0;
+            padding 0;
+        }
+        #titreAccueil{
+            text-align:center;
+        }
+        #Global #gauche{
+            float:left;
+            margin-right: auto;
+            width:25%;
+        }
+        #Global #centre{
+            float:left;
+            margin-left:auto;
+            margin-right:auto;
+            width:50%;
+        }
+        #Global #droite{
+            float: left;
+            margin-left: auto;
+            width:25%
+        }
+        #haut{
+            text-align: center;
+        }
+        footer{
+            position:absolute;
+            bottom:0;
+            left:0;
+            right:0;
+        }
     </style>
     <body>
-
-    <form action="insert_question_post.php" method="post">
-        <p>
-            <label for = "question">Question </label>
-		    <input type="text" name="question" id="question"/>
-        </p>
-
-        <p>
-            <label for = "answer">Réponse </label>
-		    <input type="text" name="answer" id="answer"/>
-        </p>
-
+    <h1 id="titreAccueil">Accueil du projet SandBoxLearn</h1>
+    <div id="haut">
         <?php
-        if(!isset($_GET['ID_list'])){
-            echo '<p>';
-            echo '<label for = "list">Liste </label>';
-            echo '<select name="list" id="list">';
-            $list = $pdo->query('SELECT * FROM tb_list WHERE list_owner_user ='.$_SESSION['ID_User']);
-            if($list->rowCount() < 1){
-                echo '<option value="error">Veuillez créer une liste</option>';
-            }else{
-                while($donnees = $list->fetch()){
-                    echo '<option value="'.$donnees['ID_list'].'">'.$donnees['list_name'].'</option>';
-                }
-            }
-            echo '</p>';
-        } else {
-            echo '<p><input type="hidden" name="list" id="list" value="' . $_GET['ID_list'] . '"/></p>';
+        if(isset($_SESSION['user'])) {
+            echo '<a href="../Compte.php">'.$_SESSION['user'].' '.'</a>';
+            echo '<a href="auth/disconnect.php">Deconnexion </a>';
+            echo '<a href="insert-list.php">Création de questionnaire </a>';
+            echo '<a href="insert_question.php">Ajout de question</a>';
+        }else {
+            echo '<a href = "auth/login.php" >Connexion </a >';
+            echo '<a href="auth/login.php">Inscription </a>';
         }
         ?>
+        <a href="">Rechercher un quizz</a>
+    </div>
+    <div id="Global">
+        <div id="droite" align="center">
+            <h2>Baniere droite</h2>
+            <p>
+                <textarea id = width="40" height="20">Ceci contiendra les tags</textarea>
+            </p>
+            <p>
+                <textarea id = "ChatBox" width="80" height="40">Chatbox</textarea>
+            </p>
+        </div>
+        <div id="centre" align="center">
+            <form action="insert_question_post.php" method="post">
+                <p>
+                    <label for = "question">Question </label>
+                    <input type="text" name="question" id="question"/>
+                </p>
 
-        <p>
-            <input type="submit" value="Envoyer" />
-	    </p>
-    </form>
+                <p>
+                    <label for = "answer">Réponse </label>
+                    <input type="text" name="answer" id="answer"/>
+                </p>
 
-<?php
+                <?php
+                if(!isset($_GET['ID_list'])){
+                    echo '<p>';
+                    echo '<label for = "list">Liste </label>';
+                    echo '<select name="list" id="list">';
+                    $list = $pdo->query('SELECT * FROM tb_list WHERE list_owner_user ='.$_SESSION['ID_User']);
+                    if($list->rowCount() < 1){
+                        echo '<option value="error">Veuillez créer une liste</option>';
+                    }else{
+                        while($donnees = $list->fetch()){
+                            echo '<option value="'.$donnees['ID_list'].'">'.$donnees['list_name'].'</option>';
+                        }
+                    }
+                    echo '</p>';
+                } else {
+                    echo '<p><input type="hidden" name="list" id="list" value="' . $_GET['ID_list'] . '"/></p>';
+                }
+                ?>
 
-if(isset($_GET['ID_list'])) {
-//Question déjà existantes
-    $reponse = $pdo->query('SELECT Question, Answer FROM tb_question, tb_list_question, tb_list WHERE ID_list=' . $_GET['ID_list'] . ' AND ID_list=list_ID_list AND question_ID_question = ID_question ORDER BY ID_Question DESC ');
-}else{
-    $reponse = $pdo->query('SELECT Question, Answer, list_name FROM tb_question, tb_list_question, tb_list WHERE ID_list=list_ID_list AND question_ID_question = ID_question ORDER BY ID_Question DESC ');
-}
+                <p>
+                    <input type="submit" value="Envoyer" />
+                </p>
+            </form>
+
+            <?php
+
+            if(isset($_GET['ID_list'])) {
+            //Question déjà existantes
+                $reponse = $pdo->query('SELECT Question, Answer FROM tb_question, tb_list_question, tb_list WHERE ID_list=' . $_GET['ID_list'] . ' AND ID_list=list_ID_list AND question_ID_question = ID_question ORDER BY ID_Question DESC ');
+            }else{
+                $reponse = $pdo->query('SELECT Question, Answer, list_name FROM tb_question, tb_list_question, tb_list WHERE ID_list=list_ID_list AND question_ID_question = ID_question ORDER BY ID_Question DESC ');
+            }
 
 
-echo '<table>';
-while ($donnees = $reponse->fetch())
-{
-    echo '<tr>';
-    echo '<td>'.htmlentities($donnees['Question']).'</td>';
-    echo '<td>'.htmlentities($donnees['Answer']).'</td>';
-    if(!isset($_GET['ID_list'])) {echo '<td>'.htmlentities($donnees['list_name']).'</td>';}
-    echo '<td><a href="">Modifier(a faire)</a></td>';
-    echo '<td><a href="">Supprimer(a faire)</a></td>';
-    echo '</tr>';
-}
-echo '</table>';
-$reponse->closeCursor();
+            echo '<table>';
+            while ($donnees = $reponse->fetch())
+            {
+                echo '<tr>';
+                echo '<td>'.htmlentities($donnees['Question']).'</td>';
+                echo '<td>'.htmlentities($donnees['Answer']).'</td>';
+                if(!isset($_GET['ID_list'])) {echo '<td>'.htmlentities($donnees['list_name']).'</td>';}
+                echo '<td><a href="">Modifier(a faire)</a></td>';
+                echo '<td><a href="">Supprimer(a faire)</a></td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+            $reponse->closeCursor();
 
-?>
+            ?>
+        </div>
+        <div id="gauche" align="center">
+            <h2>Baniere gauche</h2>
+            <p>
+                <textarea id = width="40" height="20">Ceci contiendra les tags</textarea>
+            </p>
+            <p>
+                <textarea id = "ChatBox" width="80" height="40">Chatbox</textarea>
+            </p>
+        </div>
+    </div>
+    <footer>
+        <h3>pied de page</h3>
+        <p>On peut mettre ici les crédits</p>
+    </footer>
+
 </body>
 </html>
+
+
+
